4efac7ed485286967f7aa77f66e03fc223d17192
Anailton Silva <francisco.anailton17@gmail.com>
2024-03-04 18:26:52 -0300

    feat: created serializers.py

---
+++ genres/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from genres.models import Genre
+
+
+class GenresSerializers(serializers.ModelSerializer):
+
+    class Meta:
+        model = Genre
+        fields = '__all__'
