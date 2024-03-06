63a237d4566b2a9f4660e1de233ea871dcd5c7b4
Anailton Silva <francisco.anailton17@gmail.com>
2024-03-06 16:54:44 -0300

    feat: validate fields

--- movies/serializer.py
+++ movies/serializer.py
@@ -8,3 +8,15 @@ class MovieSerilizers(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def validate_release_date(self, value):
+        if value.year < 1990:
+            raise serializers.ValidationError(
+                'A data de lançamento não pode ser anterior a 1990')
+        return value
+
+    def validate_resume(self, value):
+        if len(value) > 200:
+            raise serializers.ValidationError(
+                'Resumo não deve ser maior do que 200 carateres.')
+        return value
