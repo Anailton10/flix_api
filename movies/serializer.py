681285c03467a3b231cb798b5174908cd521fc35
Anailton Silva <francisco.anailton17@gmail.com>
2024-03-07 10:16:20 -0300

    feat: start app authentication

--- movies/serializer.py
+++ movies/serializer.py
@@ -1,22 +1,31 @@
+from django.db.models import Avg
 from rest_framework import serializers
 
 from movies.models import Movie
 
 
 class MovieSerilizers(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Movie
         fields = '__all__'
 
+    def get_rate(self, obj):
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+
+        if rate:
+            return round(rate, 1)
+        return None
+
     def validate_release_date(self, value):
-        if value.year < 1990:
+        if value.year < 1900:
             raise serializers.ValidationError(
                 'A data de lançamento não pode ser anterior a 1990')
         return value
 
     def validate_resume(self, value):
-        if len(value) > 200:
+        if len(value) > 500:
             raise serializers.ValidationError(
-                'Resumo não deve ser maior do que 200 carateres.')
+                'Resumo não deve ser maior do que 500 carateres.')
         return value
